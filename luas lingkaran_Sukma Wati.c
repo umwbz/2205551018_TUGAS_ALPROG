c2b246be5f181bec22d233e73a91ccdee4cfa990
Uma <115135900+umwbz@users.noreply.github.com>
2022-10-16 16:40:16 +0800

    Add files via upload

---
+++ luas lingkaran_Sukma Wati.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ 
+void luas() {
+  float pi, r, luas;
+  pi=3.14;
+  printf ("Masukkan jari-jari:\n");
+  scanf ("%f",&r);
+  luas=3.14*r*r;
+  printf("Luas segitiga adalah: %.2f \n",luas);
+}
+ 
+int main(void){
+  luas();
+   return 0;
+}
